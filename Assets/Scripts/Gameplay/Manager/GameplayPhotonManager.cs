187929db400a77de1f406b61eeb7f2def52fdf46
DustySkate27 <lautarolopezutn@gmail.com>
2026-09-13 22:21:07 -0300

    Fix - Game Scene Max Players

--- Assets/Scripts/Gameplay/Manager/GameplayPhotonManager.cs
+++ Assets/Scripts/Gameplay/Manager/GameplayPhotonManager.cs
@@ -22,7 +22,7 @@ public class GameplayPhotonManager : MonoBehaviourPunCallbacks
         string roomName = PhotonNetwork.CurrentRoom.Name;
         int playerCount = PhotonNetwork.CurrentRoom.PlayerCount;
 
-        if (playerCount < PhotonNetwork.CurrentRoom.MaxPlayers)
+        if (playerCount <= PhotonNetwork.CurrentRoom.MaxPlayers)
         {
             if (PhotonNetwork.IsMasterClient)
             {
